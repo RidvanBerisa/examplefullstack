27a03b217d067ee46868e340449da94c38d3227a
ridvanberisa <ridvan.berisa@protonmail.com>
2025-05-12 13:28:54 +0200

    update .gitignore and add Backend module

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [
+    // ... Deine Plugins
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        // optional: changeOrigin: true
+      }
+    }
+  }
+})
